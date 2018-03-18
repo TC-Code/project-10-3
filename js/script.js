fabd11ef4c825fcb5d6dd4e9d00a2b0a5c8b2dca
Tomasz Chowaniec <tomasz.chowaniec@icloud.com>
2018-03-18 22:22:30 +0100

    Add carousel arrows

--- js/script.js
+++ js/script.js
@@ -1,36 +1,28 @@
 $(function() {
-  var carouselList = $("#carousel ul");
-  setInterval(changeSlide, 5000);
+  var carouselList = $(".carousel__image-list");
+  var carouselInterval = setInterval(changeSlide, 5000);
   function changeSlide() {
     carouselList.animate({ marginLeft: -800 }, 1000, moveFirstSlide);
   }
   function moveFirstSlide() {
-    var firstItem = carouselList.find("li:first");
-    var lastItem = carouselList.find("li:last");
+    var firstItem = carouselList.find(".image-list__item:first");
+    var lastItem = carouselList.find(".image-list__item:last");
     lastItem.after(firstItem);
     carouselList.css({ marginLeft: 0 });
   }
-  $(".arrow-right").click(function() {
-    var firstItem = carouselList.find("li:first");
-    var lastItem = carouselList.find("li:last");
+  $(".arrow-right").on("click", function() {
+    clearInterval(carouselInterval);
     carouselList.animate({ marginLeft: -800 }, 1000, moveFirstSlide);
+    carouselInterval = setInterval(changeSlide, 5000);
   });
 
-  $(".arrow-left").click(function() {});
-});
-
-// Forward !!!!!
-/*
-$(function() {
-  $(".arrow-right").click(function() {
-    carouselList.animate({ "left": "-=800px" }, "slow" );
+  $(".arrow-left").on("click", function() {
+    clearInterval(carouselInterval);
+    var firstItem = carouselList.find(".image-list__item:first");
+    var lastItem = carouselList.find(".image-list__item:last");
+    firstItem.before(lastItem);
+    carouselList.css({ marginLeft: -800 });
+    carouselList.animate({ marginLeft: 0 }, 1000);
+    carouselInterval = setInterval(changeSlide, 5000);
   });
 });
-
-// Backwards !!!!!
-
-$(function() {
-    $(".arrow-left").click(function() {
-      alert ('Backwards !!!!!');
-    });
-  }); */
